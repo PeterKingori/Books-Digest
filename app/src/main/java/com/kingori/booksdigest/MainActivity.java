846a480492ce4fda3931544e19ef81ca39f3329a
Peter Kingori <kpndegwa@gmail.com>
2020-05-24 03:17:40 +0300

    Add arrays with the example text that will be shown on the home/main page.

--- app/src/main/java/com/kingori/booksdigest/MainActivity.java
+++ app/src/main/java/com/kingori/booksdigest/MainActivity.java
@@ -5,6 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] titles = new String[] {"Unbowed", "The Davis Dynasty", "When Breath Becomes Air"};
+    private String[] authors = new String[] {"Wangari Maathai", "John Rothchild", "Paul Kalanithi"};
+    private String[] dates = new String[] {"January 10, 2018", "May 4, 2019", "November 25, 2019"};
+    private String[] reviews = new String[] {"Unbowed, a straightforward and unfussy memoir, is most " +
+            "moving when it details the challenges this outspoken, accomplished, passionate woman faced " +
+            "in a Kenya that had no tolerance for anything other than quiet girls, " +
+            "quiet matrons, and quiet grandmothers.", "Provides a good historical overview of the investing " +
+            "landscape throughout the 20th century while focusing on the disciplined approach to investing " +
+            "followed by the Davis family, which allowed them to build their wealth throughout the ups " +
+            "and downs of the markets.", "An emotional investment well worth making: a moving and " +
+            "thoughtful memoir of family, medicine and literature. It is, despite its grim undertone, " +
+            "accidentally inspiring."};
+    private String[] favquotes = new String[] {"What people see as fearlessness is really persistence.",
+            "Bear markets make people a lot of money, they just don’t know it at the time.",
+            "You can't ever reach perfection, but you can believe in an asymptote toward which " +
+                    "you are ceaselessly striving."};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
