9f495d96d88aabc7f2d8b64b48a07a3ce6386f09
Peter Kingori <kpndegwa@gmail.com>
2020-05-24 04:05:56 +0300

    Add a toast to show the title of the book that a user has clicked on.

--- app/src/main/java/com/kingori/booksdigest/MainActivity.java
+++ app/src/main/java/com/kingori/booksdigest/MainActivity.java
@@ -3,8 +3,12 @@ package com.kingori.booksdigest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
@@ -34,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.reviewList);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
         mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String title = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, title, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
