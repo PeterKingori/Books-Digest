8285bb07eefc93a67bdb7d9bdb85bcdf43d8ad57
Peter Kingori <kpndegwa@gmail.com>
2020-05-31 03:56:47 +0300

    Implement Parcelable in ReviewInfo.java to enable data to be passed from one component to another.

--- app/src/main/java/com/kingori/booksdigest/ReviewInfo.java
+++ app/src/main/java/com/kingori/booksdigest/ReviewInfo.java
@@ -1,8 +1,10 @@
 package com.kingori.booksdigest;
 
-import androidx.annotation.NonNull;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class ReviewInfo {
+
+public class ReviewInfo implements Parcelable {
     private String mTitle;
     private String mAuthor;
     private String mDate;
@@ -15,24 +17,50 @@ public class ReviewInfo {
         this.mReview = review;
     }
 
-    public String getTitle() {
-        return mTitle;
+    protected ReviewInfo(Parcel parcel) {
+        mTitle = parcel.readString();
+        mAuthor = parcel.readString();
+        mDate = parcel.readString();
+        mReview = parcel.readString();
     }
+
+
+    public String getTitle() { return mTitle; }
 
     public String getAuthor() {
         return mAuthor;
     }
 
-    public String getDate() {
-        return mDate;
-    }
+    public String getDate() { return mDate; }
 
     public String getReview() {
         return mReview;
     }
 
     @Override
-    public String toString() {
-        return mTitle;
+    public int describeContents() {
+        return 0;
     }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mTitle);
+        parcel.writeString(mAuthor);
+        parcel.writeString(mDate);
+        parcel.writeString(mReview);
+    }
+
+    public static final Parcelable.Creator<ReviewInfo> CREATOR =
+            new Parcelable.Creator<ReviewInfo>() {
+
+                @Override
+                public ReviewInfo createFromParcel(Parcel parcel) {
+                    return new ReviewInfo(parcel);
+                }
+
+                @Override
+                public ReviewInfo[] newArray(int size) {
+                    return new ReviewInfo[size];
+                }
+            };
 }
