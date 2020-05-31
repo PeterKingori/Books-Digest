5846bf5d5cfa3960e444517c7e5e7a6bc8e7636b
Peter Kingori <kpndegwa@gmail.com>
2020-05-31 20:59:25 +0300

    Add code to get the current details of a review when a user clicks on a particular book title in MainActivity to see the details. There is also code for the 'Edit review' button with a click listener that creates a new ReviewInfo object using the current review details and puts that info into an intent extra when an intent is passed to the ReviewActivity.java class.

--- app/src/main/java/com/kingori/booksdigest/BookDetailsActivity.java
+++ app/src/main/java/com/kingori/booksdigest/BookDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,6 +17,7 @@ public class BookDetailsActivity extends AppCompatActivity {
     @BindView(R.id.authorEditText) TextView mAuthor;
     @BindView(R.id.dateEditText) TextView mDate;
     @BindView(R.id.reviewEditText) TextView mReview;
+    @BindView(R.id.addReview) Button mEditReviewButton;
 
 
     @Override
@@ -22,7 +25,27 @@ public class BookDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
         ButterKnife.bind(this);
-        
+        Intent intent = getIntent();
+
+        int position = intent.getIntExtra("position", 0);
+        final ReviewInfo selectedReview = DataManager.getInstance().getReviews().get(position);
+        final String currentTitle = selectedReview.getTitle();
+        final String currentAuthor = selectedReview.getAuthor();
+        final String currentDate = selectedReview.getDate();
+        final String currentReview = selectedReview.getReview();
+
+
+        mEditReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReviewInfo currentReviewInfo = new ReviewInfo(currentTitle, currentAuthor, currentDate,
+                        currentReview);
+                Intent intent = new Intent(BookDetailsActivity.this, ReviewActivity.class);
+                intent.putExtra(ReviewActivity.CURRENT_INFO, currentReviewInfo);
+                startActivity(intent);
+            }
+        });
+
         displayReview(mBookTitle, mAuthor, mDate, mReview);
 
     }
