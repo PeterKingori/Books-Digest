6839a1e6d9973f6d0c3db17623fb555a6636b240
Peter Kingori <kpndegwa@gmail.com>
2020-06-01 16:17:37 +0300

    Add code to enable sharing of a review via other apps using implicit intents. The code has been commented out as it is not in use currently.

--- app/src/main/java/com/kingori/booksdigest/BookDetailsActivity.java
+++ app/src/main/java/com/kingori/booksdigest/BookDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -63,4 +64,34 @@ public class BookDetailsActivity extends AppCompatActivity {
         mDate.setText(selectedReview.getDate());
         mReview.setText(selectedReview.getReview());
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_share) {
+//            share();
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private void share() {
+//        String bookTitle = mBookTitle.getText().toString();
+//        String bookAuthor = mAuthor.getText().toString();
+//        String bookReview = mReview.getText().toString();
+//        String messageText = "Check out my review for this book: " + bookTitle;
+//        Intent sendIntent = new Intent();
+//        sendIntent.setAction(Intent.ACTION_SEND);
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, messageText);
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, bookAuthor);
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, bookReview);
+//        sendIntent.setType("text/plain");
+//        Intent shareIntent = Intent.createChooser(sendIntent, null);
+//        startActivity(shareIntent);
+//    }
 }
