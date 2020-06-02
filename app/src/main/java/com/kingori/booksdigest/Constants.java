3d149ce9e2aeb28f0525e30b4311289d60e6e0f9
Peter Kingori <kpndegwa@gmail.com>
2020-06-02 08:36:16 +0300

    Add the Constants.java file to reference the Good Reads credentials for API access

---
+++ app/src/main/java/com/kingori/booksdigest/Constants.java
@@ -0,0 +1,7 @@
+package com.kingori.booksdigest;
+
+public class Constants {
+    public static final String GOODREADS_BASE_URL = "https://www.goodreads.com/";
+    public static final String GOODREADS_API_KEY = BuildConfig.GOODREADS_API_KEY;
+    public static final String GOODREADS_SECRET = BuildConfig.GOODREADS_SECRET;
+}
