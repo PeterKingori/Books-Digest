6c48aba3e114a8dad88db23954d586348c1a205a
Peter Kingori <kpndegwa@gmail.com>
2020-06-02 15:10:39 +0300

    Correct a mistake in Constants.java. Add a TextView and ListView to BookActivity.java class to show a list of movies. The views are also added in the layout file activity_book.xml

--- app/src/main/java/com/kingori/booksdigest/BookActivity.java
+++ app/src/main/java/com/kingori/booksdigest/BookActivity.java
@@ -7,13 +7,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity {
     public static final String TAG = BookActivity.class.getSimpleName();
     private Button mFindMovieButton;
     private EditText mFindBookEdit;
-    private String mBook;
+    private TextView mMovieTextView;
+    private String mBookTitle;
+    private ListView mMovieListView;
 
 
     @Override
@@ -22,11 +25,14 @@ public class BookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_book);
         mFindBookEdit = findViewById(R.id.findBookEdit);
         mFindMovieButton = findViewById(R.id.findMovie2);
+        mMovieTextView = findViewById(R.id.movieTextView);
+        mMovieListView = findViewById(R.id.movieListView);
 
         mFindMovieButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBook = mFindBookEdit.getText().toString();
+                mBookTitle = mFindBookEdit.getText().toString();
+                mMovieTextView.setText("Here are all the movies related to the book title " + mBookTitle);
             }
         });
 
--- app/src/main/java/com/kingori/booksdigest/Constants.java
+++ app/src/main/java/com/kingori/booksdigest/Constants.java
@@ -2,5 +2,5 @@ package com.kingori.booksdigest;
 
 public class Constants {
     public static final String THEMOVIEDB_BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String THEMOVIEDB_API_KEY = BuildConfig.GOODREADS_API_KEY;
+    public static final String THEMOVIEDB_API_KEY = BuildConfig.THEMOVIEDB_API_KEY;
 }
