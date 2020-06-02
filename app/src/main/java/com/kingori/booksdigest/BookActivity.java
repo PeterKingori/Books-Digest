18ac71381dd6b4e3f1e122ef1b7c64850d5aea56
Peter Kingori <kpndegwa@gmail.com>
2020-06-02 15:47:37 +0300

    Add a function that uses an array adapter to show a hardcoded array of movies. Also made changes in the Manifest file to make MainActivity.java the parent file of BookActivity.java

--- app/src/main/java/com/kingori/booksdigest/BookActivity.java
+++ app/src/main/java/com/kingori/booksdigest/BookActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,6 +18,7 @@ public class BookActivity extends AppCompatActivity {
     private TextView mMovieTextView;
     private String mBookTitle;
     private ListView mMovieListView;
+    private String[] movies = new String[] {"Invictus", "Remember the Titans", "Titanic", "The Lake House"};
 
 
     @Override
@@ -33,8 +35,14 @@ public class BookActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mBookTitle = mFindBookEdit.getText().toString();
                 mMovieTextView.setText("Here are all the movies related to the book title " + mBookTitle);
+                showMovies();
             }
         });
 
+    }
+
+    private void showMovies() {
+        ArrayAdapter moviesAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, movies);
+        mMovieListView.setAdapter(moviesAdapter);
     }
 }
